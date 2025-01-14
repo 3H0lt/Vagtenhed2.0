f7c4c8a92821d3ce9792da7bc31e8f77da62a3c6
Natalie-dk <nasr1@elev.aatg.dk>
2025-01-14 14:01:51 +0100

    Nice

--- ArduinoMainCode/ArduinoMainCode.ino
+++ ArduinoMainCode/ArduinoMainCode.ino
@@ -1,37 +1,42 @@
-#include <LCD-I2C.h>
+#include <Wire.h>
+//#include <LiquidCrystal.h>
 #include <SPI.h>
 #include <MFRC522.h>
 
-#define RST_PIN D3  // Reset Pin
-#define SS_PIN D8   // NSS pin
-#define SCL_PIN D1  // ?
-#define SDA_PIN D2  // ?
+#define RST_PIN D3    // Reset Pin
+#define SS_PIN D8     // NSS pin
+#define SCL_PIN D1    // ?
+#define SDA_PIN D2    // ?
+#define RED_PIN D0    // Red
+#define GREEN_PIN D4  // Green
 
-LiquidCrystal_I2C lcd(0x27, 16, 2);
+//LiquidCrystal_I2C lcd(0x27, 16, 2);
 MFRC522 mfrc522(SS_PIN, RST_PIN);  // Create MFRC522 instance
 String tagOLD = "";
 unsigned long lastTagTime = 0;             // Stores the time of the last detected tag
 const unsigned long clearInterval = 5000;  // Time interval to clear tagOLD (in milliseconds)
 
 void setup() {
-  lcd.begin(); // Initialize the LCD
-  lcd.backlight(); // turn on backlight
+  //lcd.begin();                        // Initialize the LCD
+  //lcd.backlight();                    // turn on backlight
   Serial.begin(115200);               // Initialiser serial kommunikation med PC
   while (!Serial) {};                 // Vent til serial er parat
   SPI.begin();                        // Initialiser SPI bus
   mfrc522.PCD_Init();                 // Initialiser MFRC522
+  RED_PIN = LOW;                      // Turn off the red led
+  GREEN_PIN = LOW;                    // Turn off the green led
   delay(4);                           // Godt at have for nogle Bards
   Serial.println(F("Scan PICC..."));  // Skriv til serial uden at bruge RAM
   //PICC betyder "Proximity Integrated Circuit Card"
-  lcd.setCursor(0, 0);
-  lcd.print("Hello, World!");
+  //lcd.setCursor(0, 0);
+  //lcd.print("Hello, World!");
 }
 void loop() {
-  delay(500); 
-  
-// rød led ikke modtager info
-// grøn led modtaget D0 og D1
-//lcd display fulde navn og nødkontakt
+  delay(500);
+
+  // rød led ikke modtager info
+  // grøn led modtaget D0 og D1
+  //lcd display fulde navn og nødkontakt
   if (millis() - lastTagTime > clearInterval) {
     tagOLD = "";  // Reset tagOLD
   }
@@ -41,6 +46,7 @@ void loop() {
   } else {
     if (!mfrc522.PICC_ReadCardSerial()) {  //Laes PICC
       Serial.println("Kan ikke laese UID");
+      RED_PIN = HIGH;
       return;
     } else {
       String tag = "";
